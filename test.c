ba53eb9ea459019fd9450aa625111cb7005d83a1
YFFCF <Ywq9972004@outlook.com>
2023-09-09 17:21:51 +0800

    print hello world

---
+++ test.c
@@ -0,0 +1,15 @@
+//
+// Created by Ywq99 on 2023/9/9.
+//
+#include <stdio.h>
+void printOut(){
+    //edit your code here
+    printf("Hello World!");
+}
+int main(){
+
+    //call the printOut function
+    printOut();
+
+    return 0;
+}
